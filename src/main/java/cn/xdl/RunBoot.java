d511bb9871f79b2e11e44bcb479d24ca5e70ca53
tom_glb <goodMorning_pro@atguigu.com>
2020-03-26 15:47:04 +0800

    right

--- src/main/java/cn/xdl/RunBoot.java
+++ src/main/java/cn/xdl/RunBoot.java
@@ -9,7 +9,7 @@ public class RunBoot {
 
 	public static void main(String[] args) {
 		ApplicationContext ac = SpringApplication.run(RunBoot.class);
-		System.out.println("right");
+		System.out.println("right11");
 //		ac.getBean("")
 	}
 	
